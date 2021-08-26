fb6a07184fbe49c55d5d26a92ca40e342fbeef9b
Eric Burlingame <burlingamesoftware@gmail.com>
2021-08-26 06:27:13 -0700

    Adding lots more topics

---
+++ components/Acronym.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+export type AcronymRowProps = {
+  letter: ReactNode;
+  title: ReactNode;
+  notes?: ReactNode;
+};
+
+const LetterCell = styled.td`
+  font-weight: 700;
+  width: 1em;
+`;
+const TitleCell = styled.td``;
+const NotesCell = styled.td``;
+
+export const AcronymRow = ({ letter, title, notes }: AcronymRowProps) => {
+  return (
+    <tr>
+      <LetterCell>{letter}</LetterCell>
+      <TitleCell>{title}</TitleCell>
+      {notes && <NotesCell>{notes}</NotesCell>}
+    </tr>
+  );
+};
+
+export type AcronymProps = {
+  children: ReactNode;
+};
+
+const AcronymTable = styled.table`
+  margin: inherit;
+`;
+
+const Acronym = ({ children }: AcronymProps) => {
+  return (
+    <AcronymTable>
+      <tbody>{children}</tbody>
+    </AcronymTable>
+  );
+};
+
+export default Acronym;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
     <Layout>
       <div>
         <h1>Outlines</h1>
-        <Link href="/outlines/commercial">commercial Outlline</Link>
+        <Link href="/outlines/commercial">Commercial Outline</Link>
       </div>
     </Layout>
   );
