7d1fcbf00ae8271586839190e4fb7ca5ce0a9449
Eric Burlingame <burlingamesoftware@gmail.com>
2021-09-02 17:53:38 -0700

    Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import Layout from "../components/Layout";
-import Link from "next/link";
+import Layout from '../components/Layout'
+import Link from 'next/link'
 
 const HomePage = () => {
-  return (
-    <Layout>
-      <div>
-        <h1>Outlines</h1>
-        <Link href="/outlines/commercial">Commercial Outline</Link>
-      </div>
-    </Layout>
-  );
-};
+    return (
+        <Layout>
+            <div>
+                <h1>Outlines</h1>
+                <Link href="/outlines/commercial">Commercial Outline</Link>
+            </div>
+        </Layout>
+    )
+}
 
-export default HomePage;
+export default HomePage
