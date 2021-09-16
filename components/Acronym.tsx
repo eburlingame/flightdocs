8f142d375c0f11b9afdd914f34ca24850181d76b
Eric Burlingame <burlingamesoftware@gmail.com>
2021-09-15 20:40:53 -0700

    Adding aeromedical factors

--- components/Acronym.tsx
+++ components/Acronym.tsx
@@ -1,45 +1,65 @@
-import { ReactNode } from "react";
-import styled from "styled-components";
+import { ReactNode } from 'react'
+import styled from 'styled-components'
 
 export type AcronymRowProps = {
-  letter: ReactNode;
-  title: ReactNode;
-  notes?: ReactNode;
-  reg?: ReactNode;
-};
+    letter: ReactNode
+    title: ReactNode
+    notes?: ReactNode
+    reg?: ReactNode
+}
 
 const LetterCell = styled.td`
-  font-weight: 700;
-  width: 1em;
-`;
-const TitleCell = styled.td``;
-const NotesCell = styled.td``;
+    font-weight: 700;
+    width: 1em;
+    text-align: center;
+    padding: 0.5em;
+`
+
+const TitleCell = styled.td`
+    width: 40%;
+    padding: 0.5em;
+`
+const NotesCell = styled.td`
+    padding: 0.5em;
+`
+
+const AcronymRowContainer = styled.tr`
+    border-radius: 0.25em;
+    transition: 0.1s background-color;
+
+    :hover {
+        transition: 0.1s background-color;
+        background-color: #efefef;
+    }
+`
 
 export const AcronymRow = ({ letter, title, notes, reg }: AcronymRowProps) => {
-  return (
-    <tr>
-      <LetterCell>{letter}</LetterCell>
-      <TitleCell>{title}</TitleCell>
-      {notes && <NotesCell>{notes}</NotesCell>}
-      {reg && <NotesCell>{reg}</NotesCell>}
-    </tr>
-  );
-};
+    return (
+        <AcronymRowContainer>
+            <LetterCell>{letter}</LetterCell>
+            <TitleCell>{title}</TitleCell>
+            <NotesCell>{notes}</NotesCell>
+            <NotesCell>{reg}</NotesCell>
+        </AcronymRowContainer>
+    )
+}
 
 export type AcronymProps = {
-  children: ReactNode;
-};
+    children: ReactNode
+}
 
 const AcronymTable = styled.table`
-  margin: inherit;
-`;
+    border: 0.5px solid #eee;
+    margin: inherit;
+    border-collapse: collapse;
+`
 
 const Acronym = ({ children }: AcronymProps) => {
-  return (
-    <AcronymTable>
-      <tbody>{children}</tbody>
-    </AcronymTable>
-  );
-};
+    return (
+        <AcronymTable>
+            <tbody>{children}</tbody>
+        </AcronymTable>
+    )
+}
 
-export default Acronym;
+export default Acronym
