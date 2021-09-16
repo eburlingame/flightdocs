b3b01f6dc85b54b0a527483363cfb296637fe338
Eric Burlingame <burlingamesoftware@gmail.com>
2021-09-15 17:38:05 -0700

    Added C172M deep dive

---
+++ components/ScenarioWrapper.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react'
+import styled from 'styled-components'
+import Layout from './Layout'
+
+export type ScenarioWrapperProps = {
+    children: ReactNode
+}
+
+const ScenarioWrapper = ({ children }: ScenarioWrapperProps) => {
+    return (
+        <Layout>
+            <div>{children}</div>
+        </Layout>
+    )
+}
+
+export default ScenarioWrapper
