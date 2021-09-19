cfe0105fb3b2a0f5f175564d8db22473ef0d7398
Eric Burlingame <burlingamesoftware@gmail.com>
2021-09-19 15:25:08 -0700

    Generate table of contents page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 const HomePage = () => {
     return (
         <Layout>
+            <Link href="/contents">All Content</Link>
             <div>
                 <h1>Outlines</h1>
                 <Link href="/outlines/commercial">Commercial Outline</Link>
