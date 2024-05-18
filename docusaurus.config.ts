99bee95aea568af7e3b9d862ca42500c01c252b9
Eric Burlingame <burlingamesoftware@gmail.com>
2024-05-18 12:34:46 -0700

    Update baseurl to https://flightdocs.io/

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://eburlingame.github.io/",
+  url: "https://flightdocs.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/flightdocs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
