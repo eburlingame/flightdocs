1cde0fc5a3298ad9c13781bb81645dedf1dcdbf0
Eric Burlingame <burlingamesoftware@gmail.com>
2025-04-11 20:47:28 -0700

    Remove and rename

--- makeSlides.sh
+++ makeSlides.sh
@@ -5,3 +5,12 @@ set -e
 mkdir -p static/slides
 find slides -type f -name '*.pdf' -exec cp {} static/slides/ \;
 
+# Recursively scan all .md files in the slides directory and convert them to .pdf using marp
+mkdir -p static/slides
+find slides -type f -name '*.md' | while read -r file; do
+    filename=$(basename "$file" .md)
+        
+    # Convert the markdown file to PDF using marp
+    echo "Converting static/slides/$filename.pdf"
+    yarn marp "$file" --pdf --pdf-notes --allow-local-files -o "static/slides/$filename.pdf"
+done
