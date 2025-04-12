3bc933bda610b540c3f039f75d4f93e086834162
Eric Burlingame <burlingamesoftware@gmail.com>
2025-04-11 21:06:29 -0700

    Hrm

--- makeSlides.sh
+++ makeSlides.sh
@@ -5,12 +5,21 @@ set -e
 mkdir -p static/slides
 find slides -type f -name '*.pdf' -exec cp {} static/slides/ \;
 
+convert_to_pdf() {
+    local file="$1"
+    local filename=$(basename "$file" .md)
+    
+    # Convert the markdown file to PDF using marp
+    marp "$file" "--pdf" "--pdf-notes" "--allow-local-files" "-o" "static/slides/$filename.pdf"
+}
+
 # Recursively scan all .md files in the slides directory and convert them to .pdf using marp
 mkdir -p static/slides
-find slides -type f -name '*.md' | while read -r file; do
+find slides -type f -name '*.md'; do
     filename=$(basename "$file" .md)
         
     # Convert the markdown file to PDF using marp
-    echo "Converting static/slides/$filename.pdf"
-    yarn marp "$file" --pdf --pdf-notes --allow-local-files -o "static/slides/$filename.pdf"
+    echo "Converting $file to static/slides/$filename.pdf"
+    marp '$file' --pdf -o static/slides/$filename.pdf
 done
+
